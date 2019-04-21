6b39d95b7b9e0ce6cef897f97329268ca5d17800
Juan Salvatore <juansalvatore.ar@gmail.com>
2019-04-21 01:18:12 -0300

    Update ejercicios

--- ejercicios/6-cadenas-de-caracteres/6.9.py
+++ ejercicios/6-cadenas-de-caracteres/6.9.py
@@ -2,13 +2,13 @@ def pedir_entero(mensaje, min, max):
     """imprime un mensaje y luego espera que el usuario ingrese un numero entero
         que se encuentre en el rango [min, max] con el minimo y el máximo incllusives.
         En caso de no cumplir el requisito aclara el rango y pide entrar un nuevo numero."""
-    numero = input(mensaje)
-    while not numero.isdigit() or int(numero) not in range(min, max + 1):
-        print(numero, not numero.isdigit(), abs(int(numero)))
-        print(f'Por favor ingresa un número entre {min} y {max}.')
+    while True:
         numero = input(mensaje)
-    return numero
+        if numero.isdigit() or ((len(numero) > 1 and numero[0] == '-') and (numero[1:].isdigit())):
+            if int(numero) >= min and int(numero) <= max:
+                return int(numero)
+        print(f'Por favor ingresa un número entre {min} y {max}.')
 
 
-edad = pedir_entero('¿cuántos años tenés? [0-150]: ', -2, 150)
+edad = pedir_entero('¿cuántos años tenés? [0-150]: ', -25, 150)
 print(edad)
