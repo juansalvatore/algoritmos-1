680b09b8915cc231795be5afa66ee0d381468e25
Juan Salvatore <juansalvatore.ar@gmail.com>
2019-05-19 02:41:36 -0300

    Update

--- ejercicios/11-objetos/11.9.py
+++ ejercicios/11-objetos/11.9.py
@@ -3,3 +3,32 @@
 
 # L = 1 -> 5 -> 8 -> 8 -> 2 -> 8
 # L.duplicar(8) => L = 1 -> 5 -> 8 -> 8 -> 8 -> 8 -> 2 -> 8 -> 8
+
+
+class _Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.prox = None
+
+
+class LE():
+    def __init__(self):
+        self.prim = None
+        self.len = 0
+
+    def insertar(self, dato, i=None):
+        if i is None:
+            i = self.len
+        if i < 0 or i > self.len:
+            raise Exception('Indice incorrecto')
+        nuevo = _Nodo(dato)
+        if i == 0:
+            nuevo.prox = self.prim
+            self.prim = nuevo
+        else:
+            act = self.prim
+            for n in range(1, i):
+                act = act.prox
+            nuevo.prox = act.prox
+            act.prox = nuevo
+        self.len += 1
