bdd0c930b8f0ed4612694839bfd89fb66ae7f433
juansalvatore <juansalvatore.ar@gmail.com>
2019-06-08 17:01:51 -0300

    Agregar ejercicios

--- ejercicios/13-recursividad/13.7.py
+++ ejercicios/13-recursividad/13.7.py
@@ -1,3 +1,57 @@
 # Ejercicio 13.7. Escribir una función que calcule recursivamente cuántos elementos hay en una
 # pila, suponiendo que la pila sólo tiene los métodos apilar y desapilar, y no altere el contenido
 # de la pila.
+
+class _Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.prox = None
+
+class Pila:
+    def __init__(self):
+        self.prim = None
+        self.len = 0
+
+    def apilar(self, dato):
+        nuevo = _Nodo(dato)
+        if self.prim is None:
+            self.prim = nuevo
+        else:
+            nuevo.prox = self.prim 
+            self.prim = nuevo
+        self.len += 1
+
+    def desapilar(self):
+        if self.prim is None:
+            raise Exception('Lista vacía')
+        dato = self.prim.dato
+        self.prim = self.prim.prox
+        self.len -= 1
+        return dato
+
+    def __str__(self):
+        arr = []
+        act = self.prim
+        while act: 
+            arr.append(str(act.dato))
+            act = act.prox 
+        return ' - '.join(arr)
+    
+def cant_elementos(pila):
+    p = pila
+    try:
+        p.desapilar()
+    except:
+        return 0
+    return 1 + cant_elementos(p)
+
+def main():
+    p = Pila()
+    p.apilar(1)
+    p.apilar(2)
+    p.apilar(3)
+    print(p)
+    print(cant_elementos(p)) # Modifica la pila actual
+    print(p)
+
+main()
--- ejercicios/13-recursividad/13.8.py
+++ ejercicios/13-recursividad/13.8.py
@@ -1,2 +1,4 @@
 # Ejercicio 13.8. Escribir una funcion recursiva que encuentre el mayor elemento de una lista.
-# 21
+
+def mayor(lista):
+    
