ad511e4eaee8fd1fa9df76a11c6346801da8edc0
juansalvatore <juansalvatore.ar@gmail.com>
2019-06-13 23:43:14 -0300

    Update ejercicios

--- prueba_parcial/ultimo_parcial/3.py
+++ prueba_parcial/ultimo_parcial/3.py
@@ -7,3 +7,58 @@
 # mismo arreglo)?
 
 # VER COMPARACIONES ENTRE METODOS DE ORDENAMIENTO
+
+
+# def quick_sort(lista):
+#     if len(lista) < 2:
+#         return lista
+#     menores, medio, mayores = _partition(lista)
+#     return quick_sort(menores) + medio + quick_sort(mayores)
+
+
+# def _partition(lista):
+#     pivote = lista[0]
+#     menores = []
+#     mayores = []
+#     for n in range(1, len(lista)):
+#         if lista[n] < pivote:
+#             menores.append(lista[n])
+#         else:
+#             mayores.append(lista[n])
+#     return menores, [pivote], mayores
+
+# def insertion_sort(lista):
+#     for i in range(1, len(lista)):
+#         val = lista[i]
+#         p = i
+#         while p > 0 and lista[p - 1] > val:
+#             lista[p] = lista[p - 1]
+#             p -= 1
+#         lista[p] = val
+#     return lista
+
+def merge_sort(lista):
+    if len(lista) < 2:
+        return lista
+    m = len(lista) // 2
+    izq = merge_sort(lista[:m])
+    der = merge_sort(lista[m:])
+    return _merge(izq, der)
+
+
+def _merge(lista1, lista2):
+    lista_ordenada = []
+    i, j = 0, 0
+    while i < len(lista1) and j < len(lista2):
+        if lista1[i] < lista2[j]:
+            lista_ordenada.append(lista1[i])
+            i += 1
+        else:
+            lista_ordenada.append(lista2[j])
+            j += 1
+    lista_ordenada += lista1[i:]
+    lista_ordenada += lista2[j:]
+    return lista_ordenada
+
+
+print(merge_sort([5, -20, 2, 52, 22, 6, 7, 29, 1]))
